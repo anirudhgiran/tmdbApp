05d0b970e890473462932b73c51fb516618c3f41
Anirudh Giran <anirudhgiran56@gmail.com>
2021-02-02 17:14:43 -0500

    Feb 2, 2021 - 05:14 PM

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChecksearchService } from '../checksearch.service';
+import { Router } from '@angular/router'
 
 //Services Import
 import { DataServiceService } from "../data-service.service";
@@ -13,9 +14,11 @@ export class NavbarComponent implements OnInit {
 
   searchText: string;
   results: any[];
+
+  isActive: boolean = false;
   
 
-  constructor(private _data: DataServiceService, private _check: ChecksearchService) { }
+  constructor(private _data: DataServiceService, private _check: ChecksearchService, private _router: Router) { }
 
   ngOnInit(): void {
     this._data.currentMessage.subscribe(message => this.searchText = message);
@@ -25,5 +28,11 @@ export class NavbarComponent implements OnInit {
     if(this.searchText === "") this._check.isEmpty = true;
     else this._check.isEmpty = false;
     this._data.changeMessage(this.searchText);
+    this._router.navigateByUrl("/search");
+    this.isActive = false;
+  }
+
+  activateBurger(){
+    this.isActive = !this.isActive;
   }
 }
